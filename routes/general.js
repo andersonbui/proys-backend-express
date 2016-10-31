0f1672232b3a5b3f248503900e69a3d46c85c992
andersonbui <andersonb@hotmail.es>
2016-10-31 03:11:19 -0500

    actualizacion con  multiples tributos

--- routes/general.js
+++ routes/general.js
@@ -66,12 +66,24 @@ router.post("/insert/:tabla", (req, res, next) => {
     });
 
 });
-
+/**
+ * ejemplo consulta al: http://localhost:8080/general/delete
+ * body:
+ * {	"nombretabla":"profesor",
+	    "nombreatributos":["nombre","oficina"],
+	    "atributos":{
+            "nombre": "aaaaa",
+            "oficina": "123"
+        }
+    }
+ */
 router.post("/delete", (req, res, next) => {
     let body = req.body;
     let tabla = body.nombretabla;
 
-    /* let consulta = "DELETE FROM " + tabla + " ";
+    /* para solo un campo 
+
+    let consulta = "DELETE FROM " + tabla + " ";
  
       if (body.nombreatributos != null && body.atributos != null && body.atributos[body.nombreatributos] != null) {
          let colname = body.nombreatributos[0];
@@ -92,12 +104,65 @@ router.post("/delete", (req, res, next) => {
         //une la condicion where al resto de la sentencia delete
         consulta = "DELETE FROM " + tabla + " WHERE " + where;
     }
-   
+
     console.log("consulta delete: " + consulta);
     //ejecuta sentencia
     req.db.query(consulta, (err, results) => {
         if (err) {
             res.status(500).send({ msg: "No se realizao el delete:", err });
+        } else {
+            if (results.affectedRows > 0) {
+                res.send(results);
+            } else {
+                res.status(404).send({ msg: " no encontrado", results });
+            }
+        }
+    });
+});
+/**
+ * ejemplo consulta a: http://localhost:8080/general/update
+ * cuerpo: 
+ * {
+	"nombretabla":"profesor",
+	"nombreatributos":["nombre","oficina"],
+	"atributos":{
+		"nombre": "aaaaa",
+		"oficina": "123"
+	},
+	"actualizacion":{
+    	"nombre": "Sergio Boraz",
+    	"telefono": "123123",
+    	"email": "sergiob@gmail.com",
+		"oficina": "1234"
+	}
+}
+ */
+router.post("/update", (req, res, next) => {
+    let body = req.body;
+    let tabla = body.nombretabla;
+    var cuerpo = {};
+
+    var consulta = "UPDATE " + tabla + " SET ? where false ";
+
+    if (body.nombreatributos != null && body.atributos != null && body.actualizacion != null) {
+        let where = "";
+        let listaatribs = [];
+        cuerpo = body.actualizacion;
+        //arma la condicon where
+        for (i = 0; i < body.nombreatributos.length; i++) {
+            let colname = body.nombreatributos[i];
+            where += (i == 0 ? " " : " and ") + body.nombreatributos[i] + " = '" + body.atributos[colname] + "'";
+
+        }
+        //une la condicion where al resto de la sentencia delete
+        consulta = "UPDATE " + tabla + " SET ? WHERE " + where;
+    }
+    //res.send({ msg: "No se realizao el update:"+consulta });
+    console.log("consulta delete: " + consulta);
+    //ejecuta sentencia
+    req.db.query(consulta,[cuerpo], (err, results) => {
+        if (err) {
+            res.status(500).send({ msg: "No se realizao el update:", err });
         } else {
             if (results.affectedRows > 0) {
                 res.send(results);
